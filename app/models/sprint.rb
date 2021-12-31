4b4c551226cf46b20b7b61c53bf9561517db6350
Christian Breitkreutz <c.breitkreutz@shipcloud.io>
2021-12-31 11:05:42 +0100

    enable encryption

--- app/models/sprint.rb
+++ app/models/sprint.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class Sprint < ApplicationRecord
+  encrypts :description, :name, :description
   validates_presence_of :iteration_id, :name, :description
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,15 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_230_201_948) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 2021_12_30_201948) do
 
-  create_table 'sprints', force: :cascade do |t|
-    t.string 'iteration_id', null: false
-    t.string 'name', null: false
-    t.string 'description', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "sprints", force: :cascade do |t|
+    t.string "iteration_id", null: false
+    t.string "name", null: false
+    t.string "description", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
+
 end
