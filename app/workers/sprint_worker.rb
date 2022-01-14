f855b63e3e0a4881322f40f47690d88a49d28347
Christian Breitkreutz <c.breitkreutz@shipcloud.io>
2022-01-14 18:05:05 +0100

    create sidekiq sprints

--- app/workers/sprint_worker.rb
+++ app/workers/sprint_worker.rb
@@ -2,6 +2,10 @@ class SprintWorker
   include Sidekiq::Worker
 
   def perform
-    puts "Hello sprint"
+    Sprint.create!(
+      iteration_id: "cron + #{SecureRandom.alphanumeric(10)}",
+      name: SecureRandom.alphanumeric(20),
+      description: SecureRandom.alphanumeric(40)
+    )
   end
 end
