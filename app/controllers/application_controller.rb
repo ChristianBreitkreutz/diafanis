e7103f10e9fdec0cb8d041e290f65004bfab09cb
Marten Klitzke <m.klitzke@shipcloud.io>
2022-02-07 09:24:35 +0100

    chore: add bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def render_flash
+    render turbo_stream: turbo_stream.update('flash', partial: 'shared/flash')
+  end
 end
